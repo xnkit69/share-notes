43c787f3ad90cc8803d258d40abd0581de96b8ee
/ A N K I T / <xnkitk69@gmail.com>
2024-02-27 18:39:31 +0530

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,23 +6,23 @@
 </script>
 
 <svelte:head>
-  <meta name="keywords" content="ephnote, e2ee, note, sharing, end-to-end, encrypted, open source, free" />
+  <meta name="keywords" content="Share Notes, e2e, note, sharing, end-to-end, encrypted, open source, free" />
   <meta property="language" content="en" />
 </svelte:head>
 
 <div class="contain">
   <div class="flex justify-between items-center mb-4">
     <a href="/">
-      <h1 class="text-2xl font-black">ephnote</h1>
-      <span class="text-xs text-neutral-800 dark:text-neutral-400">- e2ee note sharing</span>
+      <h1 class="text-2xl font-black">Share Notes</h1>
+      <span class="text-xs text-neutral-800 dark:text-neutral-400">- e2e note sharing</span>
     </a>
     <div class="flex gap-2 items-center">
       <a
-        href="https://github.com/xnkit"
+        href="https://github.com/xnkit69"
         class="p-2 rounded-full dark:hover:bg-neutral-900 hover:bg-neutral-100"
       >
         <Icon src={Github} class="w-6 h-6" />
-        <span class="sr-only">GitHub</span>
+        <span class="sr-only">GtHub</span>
       </a>
     </div>
   </div>
